4f6a3d089f426bde0a7bbf66e7bf246864d16cd2
Duong Nguyen-Huu <duongnhn@gmail.com>
2014-02-02 23:58:34 -0800

    first file

---
+++ 10001st_prime.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int checkPrime(long x)
+{
+     long i;
+     //bool check = false;
+     for (i=2;i<=round(sqrt(x));i++)
+     {
+         if ((x%i)==0)
+         {
+            return 0;
+         }
+     }
+     return 1;
+}
+
+int main()
+{
+    //cout << checkPrime(12) << endl;
+    int max = 9999;
+    long result = 3;
+    for (;max>0;max--)
+    {
+        do
+        {
+          result = result + 2;
+        }while (checkPrime(result)==0);        
+        cout << result << endl;        
+    }
+    getchar();
+    return 0;
+}
